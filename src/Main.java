5b06e60cc76d5b34cb992629beb2a192b3ac7890
aisalbek <aisalkg28@gmail.com>
2023-04-10 19:33:13 +0600

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,28 @@
+import java.util.Arrays;
+import java.util.Random;
+
+public class Main {
+    public static void main(String[] args) {
+        Random random = new Random();
+        int[]myArray = new int[10];
+        for (int i = 0; i <myArray.length ; i++) {
+           myArray[i]= random.nextInt(-5,10);
+        }
+        System.out.println(Arrays.toString(myArray));
+        boolean p =false;
+        int count=0;
+        int sum=0;
+        for (int i:myArray) {
+           if (i<0){
+               p=true;
+           }
+           if (i>0&&p==true){
+               sum+=i;
+               count++;
+
+           }
+
+        }
+        System.out.println(sum+"/"+count+"="+sum/count);
+    }
+}
